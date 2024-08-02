eda2725b9c8b07f5607a2ae15eb4cdfb4d888ca9
Subham <128891889+subhamk12@users.noreply.github.com>
2024-08-02 16:56:00 +0530

    Create sum_of_four_values.cpp

---
+++ sum_of_four_values.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define vi vector<int64_t>
+#define pb push_back
+#define ff first
+#define ss second
+#define vii vector<pair<int64_t,int64_t>>
+#define loop(i, m, n) for (int64_t i=m;i<n;i++)
+using namespace std;
+ 
+int64_t take(){
+  int64_t x;cin>>x;return x;
+}
+ 
+int32_t main()
+{
+  int64_t n;int64_t x;cin>>n>>x;
+  int64_t tot=0;
+  vii num(n,make_pair(0,0));
+  loop(i,0,n) {num[i].first=take();num[i].second=i+1;}
+  sort(num.begin(),num.end());
+  vi ans(4,0);
+  loop(h,0,n){
+    loop(i,h+1,n){
+    int64_t currsum=num[h].first+num[i].first;
+    int64_t j=i+1; int64_t k=n-1;
+    while(j<k){
+        if(num[j].first+num[k].first+currsum<x){j++;}
+        else if(num[j].first+num[k].first+currsum>x){k--;}
+        else{
+            ans[0]=num[i].second;
+            ans[1]=num[j].second;
+            ans[2]=num[k].second;
+            ans[3]=num[h].second;
+            sort(ans.begin(),ans.end());
+            cout<<ans[0]<< " "<<ans[1]<<" "<<ans[2]<<" "<<ans[3]<<endl;
+            return 0;
+        }
+    }
+  }
+  }
+  cout<<"IMPOSSIBLE"<<endl;
+  return 0;
+}
