a7027db06ab860fd7ab943c39e54bc1361768bd4
Subham <128891889+subhamk12@users.noreply.github.com>
2024-08-03 15:36:50 +0530

    Update edit_distance.cpp

--- edit_distance.cpp
+++ edit_distance.cpp
@@ -11,33 +11,33 @@ int64_t take(){
   int64_t x;cin>>x;return x;
 }
  
+int64_t getmin(int64_t x, int64_t y, int64_t z){
+  if(x>y){
+    if(y>z){return z;}
+    else{return y;}
+  }else{
+    if(z>x){return x;}
+    else{return z;}
+  }
+}
+ 
 int64_t c=1e9+7;
  
 int32_t main()
 {
-  int64_t n, m, ans;cin>>n>>m;
-  vi nums(n,0);
-  loop(i,0,n){nums[i]=take();} vector<vi> dp(n,vi(m+1,0));
-  if(nums[0]!=0) {dp[0][nums[0]]=1;}
-  else {loop(i,1,m+1) {dp[0][i]=1;}}
- 
-  loop(i,1,n){
-    if(nums[i]!=0){
-      dp[i][nums[i]]+=dp[i-1][nums[i]-1]+dp[i-1][nums[i]];
-      if(nums[i]<m){dp[i][nums[i]]+=dp[i-1][nums[i]+1];}
-      dp[i][nums[i]]%=c;
-    }else{
-      loop(j,1,m+1){
-        dp[i][j]+=dp[i-1][j-1]+dp[i-1][j];
-        if(j<m){dp[i][j]+=dp[i-1][j+1];}
-        dp[i][j]%=c;
+  int64_t n, m;string a,b;cin>>a>>b;
+  m=a.size();n=b.size();
+  vector<vi> dp(m+1,vi(n+1,0));
+  loop(i,0,n+1){dp[0][i]=i;}
+  loop(i,0,m+1){dp[i][0]=i;}
+  loop(i,1,m+1){
+    loop(j,1,n+1){
+      if(a[i-1]==b[j-1]){dp[i][j]=dp[i-1][j-1];}
+      else{
+          dp[i][j]=1+getmin(dp[i-1][j-1],dp[i-1][j],dp[i][j-1]);
       }
     }
   }
-  ans=0;
-  loop(i,1,m+1){
-    ans+=(dp[n-1][i]%c);ans%=c;
-  }
-  cout<<ans<<endl;
+  cout<<dp[m][n]<<endl;
   return 0;
-}
+               }
